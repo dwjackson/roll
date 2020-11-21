02ec14af8b6ebfb3df22319f812b68e36db3dda1
David Jackson <dwjackson@users.noreply.github.com>
2020-11-21 18:30:10 -0500

    Allow preding whitespace around custom die faces

--- src/lib.rs
+++ src/lib.rs
@@ -109,7 +109,7 @@ fn parse_values(chars: &mut std::str::Chars) -> Result<Vec<i32>, ParseRollError>
 }
 
 fn parse_value(chars: &mut std::str::Chars) -> Result<Option<i32>, ParseRollError> {
-    let c = chars.next();
+    let c = skip_whitespace(chars);
     if c.is_none() {
         return Ok(None);
     }
@@ -138,6 +138,15 @@ fn parse_value(chars: &mut std::str::Chars) -> Result<Option<i32>, ParseRollErro
         }
     }
     Err(ParseRollError::InvalidDieValue)
+}
+
+fn skip_whitespace(chars: &mut std::str::Chars) -> Option<char>{
+    while let Some(c) = chars.next() {
+        if !c.is_whitespace() {
+            return Some(c);
+        }
+    }
+    None
 }
 
 pub fn parse_rolls(s: &str) -> Result<Vec<Roll>, ParseRollError> {
@@ -217,6 +226,14 @@ mod tests {
     #[test]
     fn test_parse_custom() {
         let s = "1d{1,1,0,0,-1,-1}";
+        let roll: Roll = s.parse().expect("Bad parse");
+        assert_eq!(roll.sides, 6);
+        assert_eq!(roll.values, vec![1, 1, 0, 0, -1, -1]);
+    }
+
+    #[test]
+    fn test_parse_custom_with_preceding_space() {
+        let s = "1d{ 1,\t1, \t0,\t \t0,\t -1,   -1}";
         let roll: Roll = s.parse().expect("Bad parse");
         assert_eq!(roll.sides, 6);
         assert_eq!(roll.values, vec![1, 1, 0, 0, -1, -1]);
