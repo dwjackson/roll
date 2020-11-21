f290aee8dc225e74219ca675cedf1123203f92a4
David Jackson <dwjackson@users.noreply.github.com>
2020-11-21 16:06:26 -0500

    Show USAGE message when no arguments provided

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,11 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("USAGE: roll [DICE...]");
+        println!("e.g. roll 3d6");
+        std::process::exit(1);
+    }
     let mut dice = DiceBag::new();
     let mut total: i32 = 0;
     for s in args[1..].iter() {
